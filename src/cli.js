034dcba2148d3888882b1081b8c45adac7af1149
Krylya <krylya@inbox.ru>
2021-07-13 19:55:49 +0300

    fix brain-games

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,5 @@ import readlineSync from 'readline-sync';
 
 export default (question) => {
   const userName = readlineSync.question(question);
-  console.log(`Hi ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 };
