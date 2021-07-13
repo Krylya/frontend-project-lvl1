d29e761fe11ac2713ab9194e4af70431fa8e9fb6
Krylya <krylya@inbox.ru>
2021-07-13 20:23:46 +0300

    fix getRightAnswer

--- src/game/primeGame.js
+++ src/game/primeGame.js
@@ -2,19 +2,19 @@ import { gameInit, getRandomNumber } from '../index.js';
 
 const primeGame = () => {
   const aboutGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-  //let stringFromNum = null;
+  // let stringFromNum = null;
   let num = null;
 
   const getQuestion = () => {
     num = getRandomNumber(20);
-    //stringFromNum = String(num);
+    // stringFromNum = String(num);
     console.log(`Question: ${num}`);
   };
 
   const getRightAnswer = () => {
     let isSimply = true;
-    for (let i = 0; i < num, num -=1) {
-        if(num % i === 0) isSimply = false;
+    for (let i = 0; i < num; num -= 1) {
+      if (num % i === 0) isSimply = false;
     }
     if (isSimply) return 'yes';
     return 'no';
