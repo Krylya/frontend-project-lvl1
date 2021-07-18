0d18ba7b9d0ad59b26288cb3318cd05e04c1d7ab
Krylya <krylya@inbox.ru>
2021-07-18 20:54:10 +0300

    range from -1

--- src/game/primeGame.js
+++ src/game/primeGame.js
@@ -4,7 +4,7 @@ import gameInit from '../index.js';
 const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const primeGame = () => {
-  const num = getRandomNumber(0, 20);
+  const num = getRandomNumber(-1, 20);
 
   const getRightAnswer = () => {
     if (num === 1) return 'yes';
