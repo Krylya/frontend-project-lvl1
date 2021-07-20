4a16ce451286b608b70cc4586d07e5fbf22052fb
Krylya <krylya@inbox.ru>
2021-07-20 20:01:34 +0300

    one is no prime

--- src/game/primeGame.js
+++ src/game/primeGame.js
@@ -4,7 +4,7 @@ import gameInit from '../index.js';
 const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  if (num === 1) return true;
+  if (num === 1) return false;
   for (let i = 2; i < num; i += 1) {
     if (num % i === 0) return false;
   }
