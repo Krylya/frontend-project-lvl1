f9f258fe834d0c0cb18a3b15026249d2085e9a5a
Krylya <krylya@inbox.ru>
2021-07-20 20:00:29 +0300

    rename variables

--- src/game/progressionGame.js
+++ src/game/progressionGame.js
@@ -4,13 +4,13 @@ import gameInit from '../index.js';
 const rules = 'What number is missing in the progression?';
 const progressionLength = 10;
 
-const generate = (firstNum, deltaNum, emptyNum) => {
+const generate = (first, step, hiddenIndex) => {
   let result = '';
   for (let i = 0; i < progressionLength; i += 1) {
-    if (i === emptyNum) {
+    if (i === hiddenIndex) {
       result += '.. ';
     } else {
-      result += firstNum + deltaNum * i;
+      result += first + step * i;
       result += ' ';
     }
   }
@@ -19,12 +19,12 @@ const generate = (firstNum, deltaNum, emptyNum) => {
 
 // прогрессия
 const progressionGame = () => {
-  const firstNum = getRandomNumber(0, 10);
-  const deltaNum = getRandomNumber(0, 5);
-  const emptyNum = getRandomNumber(0, 9);
+  const first = getRandomNumber(0, 10);
+  const step = getRandomNumber(0, 5);
+  const hiddenIndex = getRandomNumber(0, 9);
 
-  const question = `Question: ${generate(firstNum, deltaNum, emptyNum)}`;
-  const rightAnswer = deltaNum * emptyNum + firstNum;
+  const question = `Question: ${generate(first, step, hiddenIndex)}`;
+  const rightAnswer = first + step * hiddenIndex;
   return [question, String(rightAnswer)];
 };
 
